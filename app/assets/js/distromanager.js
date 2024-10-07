1b1755866ffc9ab81f6d08b3d26f3ef1acceae84
shyybi <charlotte.rod@outlook.com>
2024-10-07 04:43:05 +0200

    Update owner and repo in dev-app-update.yml

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://163.5.159.110/Wall/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
